e7294a16f23df9326421fc8ebee203bb9540c705
YASAR300 <everythingdecoding@gmail.com>
2026-02-12 14:50:54 +0530

    fix: corrected font import and added tailwind 4 dark mode selector support

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from "next/font-google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import { ThemeProvider } from "@/components/ThemeProvider";
